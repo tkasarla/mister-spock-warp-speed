b9cef1bc140745bf11af663dea5066eee2b75f14
Tejaswi Kasarla <tkasarla@Tejaswis-MacBook-Air.local>
2020-12-01 19:08:59 +0530

    updated sketch.js to fix offset

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
 //  fcapture = new FCapture(); //uncomment to capture frames
 //  fcapture.startCapture(); //uncomment to capture frames
 
-  createCanvas(400, 400);
+  createCanvas(480, 480);
 
   for(let i=0;i<numStars;i++){
     stars[i] = new Star();
@@ -25,6 +25,7 @@ function draw() {
   textAlign(LEFT,TOP);
   text("WARP "+round(warp()*10)/10.0,40,40);
 
+  translate(width/2,height/2);
   for(var star of stars){
     star.update();
     star.display();
